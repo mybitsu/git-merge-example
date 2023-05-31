96ac39e4ed5d7678762f98ab9d01993fd9f2f6fa
Björn Höfling <bjoern.hoefling@mybits.de>
2023-05-31 11:26:52 +0200

    createmerge.sh: Use base branch.

--- bin/createmerge.sh
+++ bin/createmerge.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+ON_BRANCH=$1
+if [ -z "$ON_BRANCH" ]; then
+    echo "No onbranch specified. using main"
+    ON_BRANCH="main"    
+fi
+
 FEATURE=`cat data/nextfeature.txt`
 
 #echo  $FEATURE
@@ -7,7 +13,7 @@ NEXT_FEATURE=$(( 10#${FEATURE}+1 ))
 NEXT_FEATURE=$(printf "%03.f" $NEXT_FEATURE)
 echo $NEXT_FEATURE > data/nextfeature.txt
 
-git checkout main
+git checkout $ON_BRANCH
 git checkout -b feat/feature$FEATURE
 
 for COMMIT in `seq 1 3`; do
@@ -16,4 +22,4 @@ for COMMIT in `seq 1 3`; do
     git commit -m "Adding Feature ${FEATURE}_part$COMMIT."
 done    
 
-git checkout main
+git checkout $ON_BRANCH
