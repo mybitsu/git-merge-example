c92cfa8a0061d36845e405760a91cc6de0a98b6c
Björn Höfling <bjoern.hoefling@mybits.de>
2023-05-31 11:32:32 +0200

    createmerge.sh: Commit-messege from cmd.

--- bin/createmerge.sh
+++ bin/createmerge.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-ON_BRANCH=$1
+MSG=$1
+if [ -z $MSG ]; then
+    MSG="Adding Feature"
+fi
+
+ON_BRANCH=$2
 if [ -z "$ON_BRANCH" ]; then
     echo "No onbranch specified. using main"
     ON_BRANCH="main"    
@@ -19,7 +24,7 @@ git checkout -b feat/feature$FEATURE
 for COMMIT in `seq 1 3`; do
     echo "Cool feature: ${FEATURE}_part$COMMIT" >> foo.txt
     git add foo.txt
-    git commit -m "Adding Feature ${FEATURE}_part$COMMIT."
+    git commit -m "$MSG ${FEATURE}_part$COMMIT."
 done    
 
 git checkout $ON_BRANCH
