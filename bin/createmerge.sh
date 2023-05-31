f5657f04c142d2f3b6d9f2527b93eb85e1e2e910
Björn Höfling <bjoern.hoefling@mybits.de>
2023-05-31 10:27:17 +0200

    bin/createmerge.sh: git-add before commiting.

--- bin/createmerge.sh
+++ bin/createmerge.sh
@@ -12,7 +12,8 @@ git checkout -b feat/feature$FEATURE
 
 for COMMIT in `seq 1 3`; do
     echo "Cool feature: ${FEATURE}_part$COMMIT" >> foo.txt
-    git commit -am "Adding Feature ${FEATURE}_part$COMMIT."
+    git add foo.txt
+    git commit -m "Adding Feature ${FEATURE}_part$COMMIT."
 done    
 
 git checkout main
